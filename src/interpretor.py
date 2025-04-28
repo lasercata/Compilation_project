0cfac4a90fa80cd129db0d1657babe0043063c86
CelianLarrede <celian.larrede@gmail.com>
2025-04-28 10:17:07 +0200

    Some fix

--- src/interpretor.py
+++ src/interpretor.py
@@ -1,23 +1,28 @@
+import sys 
+
 class VM: 
     '''Class defining the interpretor of the object code.'''
 
     def __init__(self):  
         '''Constructor'''
+        self.debutProg()
+       
 
+    def debutProg(self):
         self.stack = []
         self.dictVariable = {}
         self.ip = 0 #Increment
-        self.ipHeap = -1 #Decrement
+        self.ipTas = -1 #Decrement
         self.base = 0
-
-    def debutProg(self):
-        pass
-
+        self.co = 0        
     def finProg(self):
-        pass
+        sys.out()
 
     def reserver(self, n: int):
-        self.stack += [None] * n
+        '''Reserve n places, supposed the heap was far away from the stack '''
+        for i in range(1,n):
+            self.stack[self.ip + i] = None
+        self.ip+=n
 
     def empiler(self, val: int):
         self.ip += 1
@@ -37,86 +42,105 @@ class VM:
         self.stack[self.stack[self.ip]] = input(">")
 
     def put(self):
-        # ça dépile aussi
-        print(self.stack.pop())
+        '''ça put et ça dépile aussi'''
+        print(self.stack[self.ip] A self.stack[self.ip] = None)
         self.ip -= 1
 
     def moins(self):
-        self.stack[-1] *= -1
+        self.stack[self.ip] *= -1
 
     def sous(self):
-        self.stack[-2] -= self.stack.pop()
+        self.stack[self.ip - 1] -= self.stack[self.ip]
+        self.stack[self.ip] = None
         self.ip -= 1
 
     def add(self):
-        self.stack[-2] += self.stack.pop()
+        self.stack[self.ip - 1] += self.stack[self.ip]
+        self.stack[self.ip] = None
         self.ip -= 1
 
     def mult(self):
-        self.stack[-2] *= self.stack.pop()
+        self.stack[self.ip - 1] *= self.stack[self.ip]
+        self.stack[self.ip] = None
         self.ip -= 1
 
     def div(self):
-        self.stack[-2] /= self.stack.pop()
+        self.stack[self.ip - 1] /= self.stack[self.ip]
+        self.stack[self.ip] = None
         self.ip -= 1
 
     def egal(self):
-        self.stack[-2] = self.stack[-2] == self.stack.pop()
+        self.stack[self.ip - 1] = self.stack[self.ip - 1] == self.stack[self.ip]
+        self.stack[self.ip] = None
         self.ip -= 1
 
     def diff(self):
-        self.stack[-2] = self.stack[-2] != self.stack.pop()
+        self.stack[self.ip - 1] = self.stack[self.ip - 1] != self.stack[self.ip]
+        self.stack[self.ip] = None
         self.ip -= 1
 
     def inf(self):
-        self.stack[-2] = self.stack[-2] < self.stack.pop()
+        self.stack[self.ip - 1] = self.stack[self.ip - 1] < self.stack[self.ip]
+        self.stack[self.ip] = None
         self.ip -= 1
 
     def infeg(self):
-        self.stack[-2] = self.stack[-2] <= self.stack.pop()
+        self.stack[self.ip - 1] = self.stack[self.ip - 1] <= self.stack[self.ip]
+        self.stack[self.ip] = None
         self.ip -= 1
 
     def sup(self):
-        self.stack[-2] = self.stack[-2] > self.stack.pop()
+        self.stack[self.ip - 1] = self.stack[self.ip - 1] > self.stack[self.ip]
+        self.stack[self.ip] = None
         self.ip -= 1
 
     def supeg(self):
-        self.stack[-2] = self.stack[-2] >= self.stack.pop()
+        self.stack[self.ip - 1] = self.stack[self.ip - 1] >= self.stack[self.ip]
+        self.stack[self.ip] = None
         self.ip -= 1
 
     def et(self):
-        self.stack[-2] = self.stack[-2] and self.stack.pop()
+        self.stack[self.ip - 1] = self.stack[self.ip - 1] and self.stack[self.ip]
+        self.stack[self.ip] = None
         self.ip -= 1
 
     def ou(self):
-        self.stack[-2] = self.stack[-2] or self.stack.pop()
+        self.stack[self.ip - 1] = self.stack[self.ip - 1] or self.stack[self.ip]
+        self.stack[self.ip] = None
         self.ip -= 1
 
     def non(self):
-        self.stack[-1] = not self.stack[-1]
+        self.stack[self.ip] = not self.stack[self.ip]
 
-    def sauter(self, adresse: int):
-        self.ip = adresse
 
-    def sauterSi(self, adresse: int):
-        if self.stack.pop() == 0: 
-            self.sauter(adresse)
+    def tra(self, adresse: int):
+        self.co = adresse
+
+    def tze(self, adresse: int):
+        
+        if self.stack[self.ip] == 0:
+            self.tra(adresse)
+        else : 
+            self.co -= 1 
+        
+        self.stack[self.ip] = None  
+        self.ip -=1 
 
     def erreur(self):
         raise Exception('Execution error')
 
     def empilerTas(self, val : int):
-        self.stack[self.ipHeap]=val
-        self.ipHeap-=1
+        self.stack[self.ipTas]=val
+        self.ipTas-=1
 
     def empilerIpTas(self):
         self.empilerAdresse(self.ip)
-        self.stack[self.ipHeap]=self.ip
+        self.stack[self.ipTas]=self.ip
 
     def empilerAdAt(self, v : int):
         '''Retrieving the dynamic address'''
         
-        self.empiler(self.ipHeap - v)
+        self.empiler(self.ipTas - v)
 
     def reserverBloc(self):
         '''Stacking the base bloc at the end of stack, what's the base block size ? '''
@@ -142,7 +166,7 @@ class VM:
         '''To do'''
 
         pass
-    
+
     def traConstr(self):
         '''To do'''
 
