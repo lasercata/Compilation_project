e74987783a5a330233ec5727635b995abf64ac0f
lasercata <67599917+lasercata@users.noreply.github.com>
2025-05-12 10:56:20 +0200

    Simplifying the compiler methods into one

--- src/compiler.py
+++ src/compiler.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 class Compiler:
+    '''Class managing the generation of the object code'''
     
     # List of object code instructions
     compiled_possible_instructions = [
@@ -9,140 +13,49 @@ class Compiler:
         "retourFonct", "retourProc", "empilerParam", "traConstr", "traVirt"
     ]
     
-    def __init__(self, fileName=None):
-        #self.outputFile = open(fileName + "_compiled.txt", "w+")
+    def __init__(self):
         self.instructions = []
-        
-    ##########################################################
-    #                   Simple instructions                  #
-    ##########################################################
-    
-    def debutProg(self):
-        self.instructions.append(["debutProg", []])
-    
-    def finProg(self):
-        self.instructions.append(["finProg", []])
-        
-    def reserver(self, n : int):
-        self.instructions.append(["reserver", [n]])
-        
-    def empiler(self, val : int):
-        self.instructions.append(["empiler", [val]])
-        
-    def empilerAdresse(self, ad : int):
-        self.instructions.append(["empilerAd", [ad]])
-    
-    def affectation(self):
-        self.instructions.append(["affectation", []])
 
-    def valeurPile(self):
-        self.instructions.append(["valeurPile", []])
-        
-    def get(self):
-        self.instructions.append(["get", []])
-        
-    def put(self):
-        self.instructions.append(["put", []])
-        
-    def moins(self):
-        self.instructions.append(["moins", []])
-        
-    def sous(self):
-        self.instructions.append(["sous", []])
-    
-    def add(self):
-        self.instructions.append(["add", []])
-        
-    def mult(self):
-        self.instructions.append(["mult", []])
-    
-    def div(self):
-        self.instructions.append(["div", []])
-    
-    def egal(self):
-        self.instructions.append(["egal", []])
-        
-    def diff(self):
-        self.instructions.append(["diff", []])
-        
-    def inf(self):
-        self.instructions.append(["inf", []])
-        
-    def infegal(self):
-        self.instructions.append(["infegal", []])
-        
-    def sup(self):
-        self.instructions.append(["sup", []])
-        
-    def supeg(self):
-        self.instructions.append(["supeg", []])
-        
-    def et(self):
-        self.instructions.append(["et", []])
-        
-    def ou(self):
-        self.instructions.append(["ou", []])
-        
-    def non(self):
-        self.instructions.append(["non", []])
-        
-    def tra(self, ad : int):
-        self.instructions.append(["tra", [ad]])
-        
-    def tze(self, ad : int):
-        self.instructions.append(["tze", [ad]])
-    
-    def erreur(self):
-        self.instructions.append(["erreur", []])
-        
-    def empilerTas(self, val : int):
-        self.instructions.append(["empilerTas", [val]])
-    
-    def empilerIpTas(self):
-        self.instructions.append(["empilerIpTas", []])
-        
-    def empilerAdAt(self, v : int):
-        self.instructions.append(["empilerAdAt", [v]])
-        
-    def reserverBloc(self):
-        self.instructions.append(["reserverBloc", []])
-        
-    def retourConstr(self):
-        self.instructions.append(["retourConstr", []])
-    
-    def retourFonct(self):
-        self.instructions.append(["retourFonct", []])
-    
-    def retourProc(self):
-        self.instructions.append(["retourProc", []])
-        
-    def empilerParam(self, ad : int):
-        self.instructions.append(["empilerParam", [ad]])
-        
-    def traConstr(self, ad : int, nbP : int):
-        self.instructions.append(["traConstr", [ad, nbP]])
-    
-    def traVirt(self, i : int, nbP : int):
-        self.instructions.append(["traVirt", [i, nbP]])
+    def add_instruction(self, name: str, *args):
+        '''
+        Adding the instruction `name` to the attribute `self.instructions`.
 
-    
+        Args:
+            name : the name of the instruction. Should be in self.compiled_possible_instructions, otherwise, a ValueError is thrown.
+            *args : the potential arguments of the instruction.
+        '''
 
-    ##########################################################
-    #                     Compiler result                    #
-    ##########################################################
-    def instructions_to_string(self):
+        if name not in self.compiled_possible_instructions:
+            raise ValueError(f'Compiler: add_instruction: name "{name}" is not a known instruction')
+    
+        self.instructions.append([name, args])
+    
+    def __str__(self) -> str:
+        '''Convert the instruction list to a string usable by the interpretor.'''
+
         string = ""
+
         for instruction in self.instructions:
-            print(instruction)
             string += instruction[0] + "("
+
             for i in range(len(instruction[1])):
                 string += str(instruction[1][i])
+
                 if i != len(instruction[1]) - 1:
                     string += ", "
+
             string += ")\n"
+
         return string
-    
-    
-            
-        
-    
+
+
+##-Tests
+if __name__ == '__main__':
+    c = Compiler()
+    c.add_instruction('debutProg')
+    c.add_instruction('empiler', 3)
+    c.add_instruction('finProg')
+
+    print(c.instructions)
+
+    print(c)
